0e11edecfbaa8ed0a83845b83ff27116af1953d0
dr0pp3r <dr0pp3rz@vk.com>
2023-12-10 00:36:14 +0300

    Рефакторинг: разделение функционала на 3 файла и перенос в папку Methods. Начало работы над поддержкой Access и MS SQL Server. Переименование программы в Database Editor.

---
+++ obzor/obzor/Methods/FileOperationMethods.cs
@@ -0,0 +1,197 @@
+﻿using ClosedXML.Excel;
+using ExcelDataReader;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace obzor.Methods
+{
+    internal class FileOperationMethods
+    {
+        Variables variables = new();
+
+        // TODO: Доработать вывод и редактирование данных в бд access
+        // Метод для открытия файла Access
+        //public static void OpenAccessFile(DataGridView dataGridView1, string path, ToolStripComboBox toolStripComboBox1, ToolStripMenuItem ToolStripMenuEditor)
+        //{
+        //    try
+        //    {
+        //        string connectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={path};";
+        //        using OleDbConnection connection = new(connectionString);
+        //        connection.Open();
+
+        //        DataTable schemaTable = connection.GetSchema("Tables");
+        //        List<string> tableNames = new();
+
+        //        foreach (DataRow row in schemaTable.Rows)
+        //        {
+        //            string tableName = row["TABLE_NAME"].ToString();
+        //            tableNames.Add(tableName);
+        //        }
+
+        //        if (tableNames.Count > 0)
+        //        {
+        //            string firstTableName = tableNames[0];
+        //            string query = $"SELECT * FROM [{firstTableName}]";
+        //            OleDbCommand command = new(query, connection);
+
+        //            OleDbDataAdapter adapter = new(command);
+        //            DataTable dataTable = new();
+        //            adapter.Fill(dataTable);
+
+        //            dataGridView1.DataSource = dataTable;
+        //            UpdateToolStripComboBox(tableNames, toolStripComboBox1, ToolStripMenuEditor);
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("База данных не содержит таблиц.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("Ошибка открытия файла Access: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //}
+
+        // TODO: Доработать вывод и редактирование данных в бд sql server
+        // Метод для открытия файла SQLServer
+        //public static void OpenSQLServerDatabase(DataGridView dataGridView1, string connectionString, ToolStripComboBox toolStripComboBox1, ToolStripMenuItem ToolStripMenuEditor)
+        //{
+        //    try
+        //    {
+        //        using SqlConnection connection = new(connectionString);
+        //        connection.Open();
+        //        DataTable schemaTable = connection.GetSchema("Tables");
+
+        //        List<string> tableNames = new();
+        //        foreach (DataRow row in schemaTable.Rows)
+        //        {
+        //            string tableName = row["TABLE_NAME"].ToString();
+        //            tableNames.Add(tableName);
+        //        }
+
+        //        if (tableNames.Count > 0)
+        //        {
+        //            string firstTableName = tableNames[0];
+        //            string query = $"SELECT * FROM [{firstTableName}]";
+        //            SqlCommand command = new(query, connection);
+
+        //            SqlDataAdapter adapter = new(command);
+        //            DataTable dataTable = new();
+        //            adapter.Fill(dataTable);
+
+        //            dataGridView1.DataSource = dataTable;
+        //            UpdateToolStripComboBox(tableNames, toolStripComboBox1, ToolStripMenuEditor);
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("База данных не содержит таблиц.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("Ошибка подключения к базе данных MS SQL Server: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //}
+
+        // TODO: Починить обновление данных при выборе другого листа
+        // Проблема заключается в том, что ToolStripComboBoxSelectedIndexChanged и LoadFile находятся в разных файлах и возможно из-за tableCollection и originalDataTable
+        // Обработчик выбора листа из выпадающего списка
+
+        // Метод обновления листов
+        public static void UpdateToolStripComboBox(List<string> tableNames, ToolStripComboBox toolStripComboBox1, ToolStripMenuItem toolStripMenuEditor)
+        {
+            toolStripComboBox1.Items.Clear();
+            if (tableNames != null && tableNames.Any())
+            {
+                foreach (string tableName in tableNames)
+                {
+                    toolStripComboBox1.Items.Add(tableName);
+                }
+                toolStripComboBox1.SelectedIndex = 0;
+                toolStripComboBox1.Enabled = true;
+                toolStripMenuEditor.Enabled = true;
+            }
+        }
+
+        // Метод для загрузки данных в DataTable из DataGridView
+        public void LoadDataIntoDataTable(DataGridView dataGridView1)
+        {
+            variables.originalDataTable = ((DataTable)dataGridView1.DataSource).Copy();
+        }
+
+        // Метод изменения состояния кнопок в режиме редактирования
+        public void UpdateButtonAvailability(DataGridView dataGridView1, ToolStripComboBox toolStripComboBox1, ToolStripMenuItem файлToolStripMenuItem, ToolStripMenuItem редактироватьToolStripMenuItem)
+        {
+            файлToolStripMenuItem.Enabled = dataGridView1.ReadOnly;
+            toolStripComboBox1.Enabled = dataGridView1.ReadOnly;
+
+            if (dataGridView1.ReadOnly)
+            {
+                редактироватьToolStripMenuItem.Text = "Редактировать";
+            }
+            else
+            {
+                редактироватьToolStripMenuItem.Text = "Выйти из режима редактирования";
+                LoadDataIntoDataTable(dataGridView1);
+            }
+        }
+
+        // Метод проверки наличия ошибок в ячейках
+        public static bool HasErrors(DataGridView dataGridView1)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    if (cell.ErrorText != null && cell.ErrorText.Trim() != "")
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        // Метод обработки ошибок, вызванных неправильно внесёнными данными в ячейки таблицы
+        public static void DataError(DataGridView dataGridView1, DataGridViewDataErrorEventArgs e)
+        {
+            DataGridViewCell errorCell = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            errorCell.ErrorText = "Ошибка в ячейке";
+
+            int displayedRow = e.RowIndex + 1;
+            int displayedColumn = e.ColumnIndex + 1;
+
+            MessageBox.Show($"Ошибка в ячейке. Номер строки: {displayedRow}, Номер столбца: {displayedColumn}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            e.ThrowException = false;
+        }
+
+        // Метод для запроса сохранений перед выбором другого файла
+        public void SaveChangesToFile(DataGridView dataGridView1)
+        {
+            if (variables.originalDataTable != null && !variables.originalDataTable.Equals((DataTable)dataGridView1.DataSource))
+            {
+                DialogResult result = MessageBox.Show("Есть несохраненные изменения. Хотите сохранить их?", "Предупреждение", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        using XLWorkbook workbook = new();
+                        var worksheet = workbook.Worksheets.Add("Лист1");
+
+                        worksheet.Cell(1, 1).InsertTable((DataTable)dataGridView1.DataSource);
+
+                        workbook.SaveAs(variables.fileName);
+
+                        MessageBox.Show("Изменения сохранены успешно.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка при сохранении изменений: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ obzor/obzor/Methods/ThemeMethods.cs
@@ -0,0 +1,58 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using ColorScheme = MaterialSkin.ColorScheme;
+
+namespace obzor.Methods
+{
+    internal class ThemeMethods
+    {
+        // Инициализация MaterialSkinManager
+        public static void MaterialSkinInit(MaterialSkinManager materialSkinManager, MaterialForm form)
+        {
+            materialSkinManager.AddFormToManage(form);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Orange800, Primary.Orange900,
+                Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE
+                );
+        }
+
+        // Обработчик события изменения MaterialSwitch для изменения темы формы
+        public static void MaterialSwitchCheckedChanged(MaterialSwitch materialSwitch1, DataGridView dataGridView1, MaterialSkinManager materialSkinManager)
+        {
+            if (materialSwitch1.Checked)
+            {
+                materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+                materialSkinManager.ColorScheme = new ColorScheme(
+                    Primary.Teal800, Primary.Teal900,
+                    Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE
+                );
+            }
+            else
+            {
+                materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+                materialSkinManager.ColorScheme = new ColorScheme(
+                    Primary.Orange800, Primary.Orange900,
+                    Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE
+                );
+            }
+
+            UpdateDataGridViewColors(dataGridView1);
+        }
+
+        // Метод обновления цвета ячеек в DataGridView
+        private static void UpdateDataGridViewColors(DataGridView dataGridView1)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    // Обновляем цвета фона и текста ячейки
+                    cell.Style.BackColor = dataGridView1.DefaultCellStyle.BackColor;
+                    cell.Style.ForeColor = dataGridView1.DefaultCellStyle.ForeColor.Darken(1);
+
+                }
+            }
+        }
+    }
+}
